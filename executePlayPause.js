71598ff0c92a0d20bd71afa740a90927b374ca49
aknobloch <aaronknobloch92@gmail.com>
2017-12-22 09:57:21 -0500

    proper play/pause toggle logic

--- executePlayPause.js
+++ executePlayPause.js
@@ -1,1 +1,11 @@
-document.getElementsByClassName("play-btn")[0].click()
+var playButton = document.getElementsByClassName("play-btn")[0];
+
+if(playButton && ! playButton.classList.contains("hidden"))
+{
+    playButton.click()
+}
+else
+{
+    document.getElementsByClassName("pause-btn")[0].click()
+}
+
